b92908bafe9f782371e8b54be5752ada549314c2
Sorn Tangmettri <123283256+Sorn4web@users.noreply.github.com>
2024-11-01 20:50:20 +0700

    [ADD] daisyUI and config brand colors and font Inter

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,17 +1,5 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,11 +13,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap"
+          rel="stylesheet"
+        />
+      </head>
+      <body className="font-inter">{children}</body>
     </html>
   );
 }
--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,19 +1,36 @@
-import type { Config } from "tailwindcss";
+import { Config } from "tailwindcss";
+import daisyui from "daisyui";
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+      },
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        green: {
+          100: "#D8E9E4",
+          300: "#2B5F44",
+          500: "#243831",
+        },
+        golden: "#C5A365",
+        black: "#000000",
+        white: "#FFFFFF",
+        text: "#191919",
+        grey: {
+          100: "#BBC2C0",
+          300: "#939494",
+        },
+        success: "#49A569",
       },
     },
   },
-  plugins: [],
+  plugins: [daisyui],
 };
+
 export default config;
